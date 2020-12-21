8caca527c17c1a49b06e990f6c203d97eb5be811
orloveste <orlovevm@yahoo.com>
2020-12-21 14:53:10 +0200

    Initial commit

---
+++ src/main/java/demoTrackerDrone/demoTrackerDroneapp/DemoTrackerDroneAppApplication.java
@@ -0,0 +1,13 @@
+package demoTrackerDrone.demoTrackerDroneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrackerDroneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTrackerDroneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demoTrackerDrone/demoTrackerDroneapp/DemoTrackerDroneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demoTrackerDrone.demoTrackerDroneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTrackerDroneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
